929a1f556b1737a21bebe091d69743d49edce638
Leonardo Melo <leonardo.melo@caju.com.br>
2025-04-07 12:16:35 -0300

    chore: add eslint config and automation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
-  return (
-    <div data-testid="app-container">
-      <h1>Hiring front end</h1>
-    </div>
-  );
+    return (
+        <div data-testid="app-container">
+            <h1>Hiring front end</h1>
+        </div>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import './styles/reset.scss'
+import App from './App.tsx';
+
+import './styles/reset.scss';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
