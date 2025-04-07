eca94861406e1ad1b69d2b8c13b6f145f68fb0da
Leonardo Melo <leonardo.melo@caju.com.br>
2025-04-07 12:07:24 -0300

    chore: add sass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,9 @@ import App from './App';
 describe('App', () => {
   test('renders without crashing', () => {
     render(<App />);
+
     const appElement = screen.getByTestId('app-container');
+
     expect(appElement).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 export default function App() {
   return (
     <div data-testid="app-container">
-      <h1>My App</h1>
+      <h1>Hiring front end</h1>
     </div>
   );
 }
